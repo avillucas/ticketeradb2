7fcb22a60724e3392c50420d7f38252af63290f6
Lucas Avila <avillucas@gmail.com>
2023-11-25 03:38:30 -0300

    correcciones

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const express = require('express');
 //
 const port = process.env.PORT;
 const app = express();
-app.use(express.json);
+app.use(express.json());
 connectDB()
-//routes
+
+//todos los tickets
 app.get('/api/v1/tickets', async (req, res) => {
     try {
         const data = await TicketModel.find();
@@ -22,6 +23,7 @@ app.get('/api/v1/tickets', async (req, res) => {
         });
     }
 });
+
 app.listen(port, () => {
     console.log("Server is running in por " + port);
 })
